3a2c05c684765a8be97808229f818242d3f051e6
Firat Yedibela <yedibelafirat0@gmail.com>
2024-06-10 15:08:02 +0300

    Add Gameboard class and its ship placement logic

---
+++ src/__tests__/gameboard.test.js
@@ -0,0 +1,250 @@
+import Gameboard from '../modules/gameboard';
+
+describe('gameboard', () => {
+  describe('board', () => {
+    let gameboard = new Gameboard(10);
+    it('should have a board property', () => {
+      expect(gameboard.board).toBeDefined();
+    });
+
+    it('should have Size x Size cells', () => {
+      // Test 10x10
+      let length = gameboard.board.length * gameboard.board[0].length;
+      expect(length).toBe(100);
+
+      // Test 15x15
+      gameboard = new Gameboard(15);
+      length = gameboard.board.length * gameboard.board[0].length;
+      expect(length).toBe(225);
+    });
+  });
+
+  describe('placeShip', () => {
+    // Reset board after each test block
+    let gameboard;
+    beforeEach(() => {
+      gameboard = new Gameboard(10);
+    });
+
+    it('should place a ship with length 1 properly', () => {
+      // Test
+      let row = 1;
+      let col = 2;
+      let length = 1;
+      gameboard.placeShip(row, col, length, true);
+      let actual = gameboard.board;
+      let expected = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 'S', 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ];
+      expect(actual).toEqual(expected);
+
+      // Test
+      row = 6;
+      col = 8;
+      length = 1;
+      gameboard.placeShip(row, col, length, true);
+      actual = gameboard.board;
+      expected = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 'S', 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 'S', 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ];
+      expect(actual).toEqual(expected);
+    });
+
+    it('should place a ship with a length more than 1 properly', () => {
+      // Test with length 3
+      let row = 0;
+      let col = 0;
+      let length = 3;
+      gameboard.placeShip(row, col, length, true);
+      let actual = gameboard.board;
+      let expected = [
+        ['S', 'S', 'S', 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ];
+      expect(actual).toEqual(expected);
+
+      // Test with length 5 and different starting point
+      row = 2;
+      col = 2;
+      length = 5;
+      gameboard.placeShip(row, col, length, true);
+      actual = gameboard.board;
+      expected = [
+        ['S', 'S', 'S', 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 'S', 'S', 'S', 'S', 'S', 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ];
+      expect(actual).toEqual(expected);
+    });
+
+    it('should place a ship through correct axis', () => {
+      // Test vertical
+      let row = 2;
+      let col = 2;
+      let length = 3;
+      gameboard.placeShip(row, col, length, false);
+      let actual = gameboard.board;
+      let expected = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 'S', 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 'S', 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 'S', 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ];
+      expect(actual).toEqual(expected);
+
+      // Test horizontal
+      row = 2;
+      col = 4;
+      length = 3;
+      gameboard.placeShip(row, col, length, true);
+      actual = gameboard.board;
+      expected = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 'S', 0, 'S', 'S', 'S', 0, 0, 0],
+        [0, 0, 'S', 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 'S', 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ];
+      expect(actual).toEqual(expected);
+    });
+
+    it('should prevent ships with length 1 from overlapping', () => {
+      // board[4][4] is not empty
+      gameboard.board = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 'S', 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ];
+      expect(() => {
+        gameboard.placeShip(4, 4, 1, true);
+      }).toThrow('Cell already occupied!');
+    });
+
+    it('should prevent ships from horizontal overlapping', () => {
+      // Horizontal overlapping, [5][5] is not empty
+      gameboard.board = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 'S', 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ];
+      expect(() => {
+        gameboard.placeShip(5, 3, 4, true);
+      }).toThrow('Cell already occupied!');
+    });
+
+    it('should prevent ships from vertical overlapping', () => {
+      // Vertical overlapping, [5][5] is not empty
+      gameboard.board = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 'S', 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ];
+      expect(() => {
+        gameboard.placeShip(3, 5, 4);
+      }).toThrow('Cell already occupied!');
+    });
+
+    it('should prevent ships from horizontal overflowing', () => {
+      // Horizontal overflow
+      gameboard.board = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ];
+      expect(() => {
+        gameboard.placeShip(0, 7, 4, true);
+      }).toThrow('Out of bounds!');
+    });
+
+    it('should prevent ships from vertical overflowing', () => {
+      // Vertical overflowing
+      gameboard.board = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ];
+      expect(() => {
+        gameboard.placeShip(7, 0, 4, false);
+      }).toThrow('Out of bounds!');
+    });
+  });
+});
---
+++ src/modules/gameboard.js
@@ -0,0 +1,50 @@
+import Ship from '../modules/ship';
+
+class Gameboard {
+  constructor(size) {
+    // Create board as 2D array
+    this.board = Array(size)
+      .fill()
+      .map(() => Array(size).fill(0));
+  }
+
+  checkIfCellAvailable(row, col) {
+    // Overflow case
+    if (row > 9 || col > 9) {
+      throw new Error('Out of bounds!');
+    }
+    // Overlap case
+    else if (this.board[row][col] !== 0) {
+      throw new Error('Cell already occupied!');
+    }
+  }
+
+  checkAllCells(row, col, length, isHorizontal) {
+    for (let i = 0; i < length; i++) {
+      // The axis alongh which we're placing the ship remains constant
+      // The other axis increases with each iteration
+      const currentRow = isHorizontal ? row : row + i;
+      const currentCol = isHorizontal ? col + i : col;
+      this.checkIfCellAvailable(currentRow, currentCol);
+    }
+  }
+
+  placeShip(row, col, length, isHorizontal) {
+    const ship = new Ship(length);
+    if (length === 1) {
+      this.checkIfCellAvailable(row, col);
+      this.board[row][col] = 'S';
+    }
+    // Length more than 1
+    else {
+      this.checkAllCells(row, col, length, isHorizontal);
+      for (let i = 0; i < length; i++) {
+        const currentRow = isHorizontal ? row : row + i;
+        const currentCol = isHorizontal ? col + i : col;
+        this.board[currentRow][currentCol] = 'S';
+      }
+    }
+  }
+}
+
+export default Gameboard;
